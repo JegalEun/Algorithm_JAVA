a197cfaa20fabf50e08ef59a5d024de36c73b58a
Eun <43868540+JegalEun@users.noreply.github.com>
2021-12-30 11:53:46 +0900

    [Update] BOJ1676_팩토리얼 0의 개수

---
+++ Week/Week05/BOJ1676.java
@@ -0,0 +1,27 @@
+package Week05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1676 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		int count=0;
+		
+		while(num>=5) {
+			count+=num/5;
+			num/=5;
+		}
+
+		System.out.println(count);
+
+	}
+
+}
