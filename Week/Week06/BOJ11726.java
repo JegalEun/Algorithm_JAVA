a3151830bde7862d1e6d73f4c021fcc882ca91bd
Eun <43868540+JegalEun@users.noreply.github.com>
2022-01-07 17:38:36 +0900

    [Update] BOJ11726_2xn 타일링

---
+++ Week/Week06/BOJ11726.java
@@ -0,0 +1,29 @@
+package Week06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11726 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int []dp = new int[n+2];
+		
+		dp[1]=1;
+		dp[2]=2;
+		
+		for(int i=3;i<=n;i++) {
+			dp[i]=dp[i-1]+dp[i-2];
+			dp[i]%=10007;
+		}
+		
+		System.out.println(dp[n]);
+
+	}
+
+}
