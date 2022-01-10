37c0154eb13cfc96a18827673604e8584513d824
Eun <43868540+JegalEun@users.noreply.github.com>
2022-01-10 13:07:06 +0900

    [Update] BOJ11052_카드 구매하기 (코드수정)

--- Week/Week06/BOJ11052.java
+++ Week/Week06/BOJ11052.java
@@ -7,14 +7,15 @@ import java.util.StringTokenizer;
 
 public class BOJ11052 {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int n = Integer.parseInt(br.readLine());
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
 		int [] p = new int[n+1]; 
 		int [] dp = new int[n+1];
 		
@@ -24,11 +25,12 @@ public class BOJ11052 {
 		
 		
 		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n;j++) {
+			for(int j=1;j<=i;j++) {
 				dp[i] = Math.max(dp[i],p[j]+dp[i-j]);
 			}
 		}
 		
+		System.out.println(dp[n]);
 
 	}
 
