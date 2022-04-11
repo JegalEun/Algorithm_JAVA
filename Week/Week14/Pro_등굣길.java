984a6cfebd1a867f5aa611397c7a3c4a81340c30
Eun <43868540+JegalEun@users.noreply.github.com>
2022-04-11 18:51:57 +0900

    [Update] Programmers_등굣길 (주석 추가)

--- Week/Week14/Pro_등굣길.java
+++ Week/Week14/Pro_등굣길.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
@@ -8,15 +6,18 @@ class Solution {
         
         for(int i=0;i<puddles.length;i++) {
         	street[puddles[i][1]][puddles[i][0]]=-1;
+        	// 웅덩이는 -1로 표시
         }
         
         street[1][1]=1;
         
         for(int i=1;i<n+1;i++) {
         	for(int j=1;j<m+1;j++) {
+        		// 웅덩이라면
               	if(street[i][j]==-1) continue;
-              	if(street[i-1][j]>0) street[i][j]+=street[i-1][j]%mod;
-              	if(street[i][j-1]>0) street[i][j]+=street[i][j-1]%mod;
+              	// 웅덩이가 아니라면
+              	if(street[i-1][j]>0) street[i][j]+=street[i-1][j]%mod; // 위
+              	if(street[i][j-1]>0) street[i][j]+=street[i][j-1]%mod; // 왼쪽
         	}
         }
         
