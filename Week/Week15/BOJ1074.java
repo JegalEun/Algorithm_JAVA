ac83c5f8745f006546d5bf5ed6fa2e059908b258
Eun <43868540+JegalEun@users.noreply.github.com>
2022-04-19 16:50:34 +0900

    [Update] BOJ1074_Z

---
+++ Week/Week15/BOJ1074.java
@@ -0,0 +1,51 @@
+package Week15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1074 {
+
+	static int count=0;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int n = (int) Math.pow(2, N);
+	
+		find(n,r,c);
+		System.out.println(count);
+
+	}
+	
+	public static void find(int size, int r, int c) {
+		if(size==1)
+			return;
+		
+		if(r<size/2 && c<size/2) {
+			//1사분면에 있을 경우
+			find(size/2,r,c);
+		}else if(r<size/2 && c>=size/2) {
+			// 2사분면에 있을 경우
+			count+=size*size/4;
+			find(size/2, r, c-size/2);
+		}else if(r >= size/2 && c<size/2) {
+			// 3사분면에 있을 경우
+			count += (size*size/4)*2;
+			find(size/2, r-size/2, c);
+		}else {
+			// 4사분면에 있을 경우
+			count+=(size*size/4)*3;
+			find(size/2, r-size/2, c-size/2);
+		}
+	}
+
+}
