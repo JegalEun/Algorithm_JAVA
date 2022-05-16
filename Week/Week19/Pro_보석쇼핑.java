5bc48c55cee8d1e1cdad319ec419cad3ea6a23b5
Eun <43868540+JegalEun@users.noreply.github.com>
2022-05-16 15:19:23 +0900

    [Hash] Programmers_보석 쇼핑

---
+++ Week/Week19/Pro_보석쇼핑.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] gems) {
+        
+        HashMap<String, Integer> hm = new HashMap<>();
+        HashSet<String> hs = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        int start = 0;
+        int startPoint=0;
+        int end = Integer.MAX_VALUE;
+        
+        int[] answer = {};
+        
+        // hashset에 보석의 종류 개수 세기
+        for(int i=0;i<gems.length;i++) {
+        	hs.add(gems[i]);
+        }
+       
+        //	보석의 종류과 개수 넣기
+        for(int i=0;i<gems.length;i++) {
+        	hm.put(gems[i], hm.getOrDefault(gems[i],0)+1);
+        	
+        	q.add(gems[i]);
+        	
+        	while(true) {
+        		String temp = q.peek();
+        		if(hm.get(temp)>1) {
+        			//2이상 있으면
+        			q.poll();
+        			start++;
+        			hm.put(temp, hm.get(temp)-1);
+        		}else break;
+        	}
+        	
+        	if(hm.size() == hs.size() && end > q.size()) {
+        		end = q.size();
+        		startPoint = start;
+        	}
+        	
+        }
+        
+        return new int[] {startPoint+1, startPoint+end}; 
+    }
+}
